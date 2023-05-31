a50d354bbfdf412cc8ee8890f989cc79d3aadab5
Joakim Vanebo <Joakimvanebo1@gmail.com>
2023-05-31 23:41:03 +0200

    added content

---
+++ src/helpers/ProjectList.jsx
@@ -0,0 +1,42 @@
+import Img1 from "../assets/images/semester-project-2.png";
+import Img2 from "../assets/images/javascript-framework.png";
+import Img3 from "../assets/images/javascript-2-Noroff.png";
+import Img4 from "../assets/images/vaghaugen.png";
+
+export const ProjectList = [
+    {
+      id:"/Pjatte1337/holidaze-exam-project",
+      name: "Holidaze Accommodation",
+      title: "Project Exam 2 ",
+      info: "Noroff School of technology and digital media",
+      image: Img1,
+      description: "This is my Project Exam 2 @ Noroff - School of technology and digital media. The website is an accommodation website using Noroff's live API. On the Holidaze website you can view, post and book venues for specific dates.",
+      tools: "This project is designed with prototype in Figma. built with React (<16), BootStrap 5, StyledComponents and CSS modules.",
+      github:"/Pjatte1337/holidaze-exam-project",
+    },
+    {
+      name: "StoreTown Ecommerce",
+      title: "JavaScript Frameworks",
+      info: "Noroff School of technology and digital media",
+      image: Img2,
+      description: "This is my JavaScript Framework Assignment @ Noroff - School of technology and digital media. The website is an eCom store using Noroff's live API. On the StoreTown eCom store you can buy product in different category.",
+      tools: "This project is built with React(<6), Bootstrap 5, StyledComponent, Zustand, CSS Module And Noroff API",
+      github:"<a href='www.google.com'",
+    },
+    {
+      name: "Social Media Platform",
+      title: "JavaScript Project 2",
+      info: "Noroff School of technology and digital media",
+      image: Img3,
+      description: "This is my JavaScript 2 Assignment @ Noroff - School of technology and digital media. The website is an Social Media Platform using Noroff's live API. On the platform you can viewing, posting, editing and deleting social media content.",
+      tools: "This project is built with HTML, CSS Module, JavaScript And Noroff API",
+    },
+    {
+      name: "Våghaugen Accommodation",
+      title: "",
+      info: "A private project for my familiy",
+      image: Img4,
+      description: "JavaScript,HTML,CSS",
+      tools: "JavaScript,HTML,CSS",
+    },
+  ];
