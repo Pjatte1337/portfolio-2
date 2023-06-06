0f4ef2c1b818fa1da287be41415f9588d2d85cd1
Joakim Vanebo <Joakimvanebo1@gmail.com>
2023-06-06 03:12:25 +0200

    test

--- src/pages/ProjectDisplay.jsx
+++ src/pages/ProjectDisplay.jsx
@@ -50,7 +50,7 @@ function ProjectDisplay() {
             className="btn-links"
             onClick={() => handleButtonClick(project.github)}
           >
-            Github
+            ssss
           </button>
         </div>
       </div>
